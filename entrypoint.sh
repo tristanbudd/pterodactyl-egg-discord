ad32ea76eef9682ae102d48e88c2f464c1c157b7
stanislasbdx <s.castaybert@deal.fr>
2022-12-24 23:09:47 +0100

    ⚪ other・Yarn installs only prod dependencies and with a cache verification

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bashll
+yarn install --check-cache --production=true
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-yarn install
 ${MODIFIED_STARTUP}
