14764b15d1041809d2b8e1cfc9e4ed02d8535d18
stanislasbdx <s.castaybert@deal.fr>
2023-06-21 12:03:18 +0200

    🚀 workspace upgrade・Changed production flag on yarn install

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yarn install --check-cache --production=true
+yarn install --check-cache --production
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 ${MODIFIED_STARTUP}
