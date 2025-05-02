05c4f3b4809c3907b0cc53bbb2695a819f15191f
stanislasbdx <s.castaybert@deal.fr>
2025-05-02 23:50:03 +0200

    🔊 Adding debug logs, removed --production and added --immutable param for yarn install

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,17 @@
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
+echo "> cd /home/container"
 cd /home/container
 
+echo "> corepack enable"
 corepack enable
 
-yarn install --check-cache --production
+echo "> yarn set version stable"
+yarn set version stable
+
+echo "> yarn install --immutable --check-cache"
+yarn install --immutable --check-cache
 
 echo "-- Server started, waiting for ${STARTUP_FILE}..."
 
