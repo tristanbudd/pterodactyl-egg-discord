04e35e75d24a7ca4d6785acdc42cf2167ff12707
Tristan <tristanbudd@outlook.com>
2025-07-01 13:52:57 +0100

    Set yarn to default.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 
-timestamp() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')]"
-}
-
-log() {
-    echo "$(timestamp) $*"
-}
-
-# Replace {{VAR}} with ${VAR} so environment variables get expanded by bash -c
-MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
-log "Info | Enabling Corepack..."
 corepack enable
 
-log "Info | Changing directory to /home/container"
-cd /home/container || { log "Error | Failed to change directory to /home/container"; exit 1; }
+echo "$ cd /home/container"
+cd /home/container
 
-log "Info | Setting Yarn linker to node-modules"
+echo "$ yarn config set nodeLinker node-modules"
 yarn config set nodeLinker node-modules
 
-log "Info | Installing dependencies with Yarn..."
+echo "$ yarn install"
 yarn install
 
-log "Info | Server startup sequence initiated. Waiting for ${STARTUP_FILE}..."
+echo "-- Server started, waiting for ${STARTUP_FILE}..."
 
-# Directly execute the startup command in bash -c (no echo, no eval echo)
-exec bash -c "$MODIFIED_STARTUP"
+${MODIFIED_STARTUP}
